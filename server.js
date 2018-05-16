b3c3802fd2bc3ff4bd65066077058e5f254d891b
Yash Patel <ypatel@princeton.edu>
2018-05-16 12:56:48 -0400

    Changed baseline

---
+++ server.js
@@ -0,0 +1,6 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 5000, function(){
+    console.log('Server running on 5000...');
+});
